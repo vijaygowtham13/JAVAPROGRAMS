2cb1cc9fde6d221373453386c8755ccdab6ad1a8
VIJAYGOWTHAM.K <128630609+vijaygowtham13@users.noreply.github.com>
2023-12-27 17:55:50 +0530

    Add files via upload
    This is a simple program to create and print an array

---
+++ array.java
@@ -0,0 +1,19 @@
+
+class array{
+public static void main(String[] args){
+   
+int[] a = new int[5];
+int k=1;
+for(int i=0;i<a.length;i++){
+a[i]=k;
+k++;
+}
+for(int i=0;i<a.length;i++){
+System.out.println(a[i]);
+}
+
+}
+
+
+
+}
