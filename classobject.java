e9ff5284599ec8c0891db157d7d1dffd095f52a6
VIJAYGOWTHAM.K <128630609+vijaygowtham13@users.noreply.github.com>
2023-12-27 18:27:08 +0530

    Classes and Objects Finding The Area Of Box

---
+++ classobject.java
@@ -0,0 +1,19 @@
+//classes and objects finding the area of box
+class classobject{
+public static void main(String ar[]){
+ cube obj = new cube();
+ obj.w = 2;
+ obj.h=2;
+ obj.d=4;
+ int d = obj.w*obj.h*obj.d;
+System.out.println(d);
+
+}
+
+}
+class cube{
+ int w;
+ int h;
+ int d;
+
+}
